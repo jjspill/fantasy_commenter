10a34f29ea47e14107793bfb1feef6cb2b63b952
James Spillmann <james@spacinsider.com>
2024-08-12 09:50:14 -0600

    rankings scraper better

--- src/fantasy_ai/rankings_service/main.py
+++ src/fantasy_ai/rankings_service/main.py
@@ -4,19 +4,22 @@ from rankings.fantasypros import FantasyProsScraper
 
 configurations = {
     "dynasty-superflex": "https://www.fantasypros.com/nfl/rankings/dynasty-superflex.php",
+    "dynasty-1qb": "https://www.fantasypros.com/nfl/rankings/dynasty-overall.php",
+    "redraft-ppr": "https://www.fantasypros.com/nfl/rankings/ppr-cheatsheets.php",
 }
 
 
 async def main():
-    config = "dynasty-superflex"
-    print(f"Fetching data for {config}...")
-    scraper = FantasyProsScraper(configurations[config])
-    html = await scraper.fetch_data()
-    raw_data = scraper.extract_data(html)
-    if raw_data:
-        players = scraper.parse_data(raw_data)
-        await scraper.write_to_db(players)
-        print("Data written to database.")
+    configs = ["dynasty-superflex", "dynasty-1qb", "redraft-ppr"]
+    for config in configs:
+        print(f"Fetching data for {config}...")
+        scraper = FantasyProsScraper(configurations[config])
+        html = await scraper.fetch_data()
+        raw_data = scraper.extract_data(html)
+        if raw_data:
+            players = scraper.parse_data(raw_data)
+            await scraper.write_to_db(players, config)
+            print("Data written to database.")
 
 
 if __name__ == "__main__":
--- src/fantasy_ai/rankings_service/rankings/fantasypros.py
+++ src/fantasy_ai/rankings_service/rankings/fantasypros.py
@@ -15,7 +15,6 @@ db = firestore.Client()
 @dataclass
 class Fantasypros_Player:
     name: str
-    age: int
     position: str
     rank_avg: int
     pos_rank: str
@@ -42,7 +41,6 @@ class FantasyProsScraper(RankingsScraper):
         players = [
             Fantasypros_Player(
                 name=item["player_name"],
-                age=int(item["player_age"]),
                 position=item["player_position_id"],
                 rank_avg=float(item["rank_ave"]),
                 pos_rank=item["pos_rank"],
@@ -52,24 +50,26 @@ class FantasyProsScraper(RankingsScraper):
         ]
         return players
 
-    async def write_to_db(self, players):
+    async def write_to_db(self, players, config_slug):
         print("Writing to database...")
         batch = db.batch()
-        rankings_col_ref = db.collection("rankings")
+        rankings_col_ref = db.collection("player_info")
         current_date, current_time = get_dates()
 
         for player in players:
             player_id = get_player_id(player.name)
             player_doc_ref = rankings_col_ref.document(player_id)
+            fantasypros_doc_ref = player_doc_ref.collection("rankings").document(
+                "fantasypros-" + config_slug
+            )
 
-            fantasypros_player_col_ref = player_doc_ref.collection("fantasypros")
-            date_doc_ref = fantasypros_player_col_ref.document(current_date)
-
-            player_data = player.__dict__
-            player_data.update({"ranking_date": current_time})
-
-            batch.set(date_doc_ref, player_data)
+            new_player_data = player.__dict__
+            new_player_data["date"] = current_date
+            batch.set(
+                fantasypros_doc_ref,
+                {"rankings_data": firestore.ArrayUnion([new_player_data])},
+                merge=True,
+            )
 
         batch.commit()
-
         print("Data written successfully.")
