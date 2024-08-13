3c3ba1abcf3a437173862cfcb7415ffe9bc8bd0b
James Spillmann <james@spacinsider.com>
2024-08-13 16:37:01 -0600

    data intake now working for wr

--- sleeper_service/src/sleeper_api_cliient.py
+++ sleeper_service/src/sleeper_api_cliient.py
@@ -1,11 +1,22 @@
 import asyncio
+from typing import Any, Dict, List
 
 import aiohttp
 from google.cloud import firestore
 
 from src.config import Mode
-from src.types import SleeperPlayer
-from src.utils import extract_news, extract_sleeper_profile, filter_players
+from src.types import (
+    SleeperHistoricalStats,
+    SleeperPlayer,
+    SleeperWRStatsWeekly,
+    SleeperWRYearlyStats,
+)
+from src.utils import (
+    extract_news,
+    extract_sleeper_profile,
+    extract_stats,
+    filter_players,
+)
 
 ## Thinking
 ## Fetch all the data from sleeper APIs
@@ -26,12 +37,24 @@ class SleeperAPIClient:
         await self._fetch_player_data()
 
     async def _create_player(self, player_id, player_info, session):
-        print("Fetching player news for", player_id)
         profile = extract_sleeper_profile(player_info)
-        news = await self._fetch_player_news(player_id, session)
+        historical_stats = None  # Default to None
+
+        if profile.position == "WR":  # Only fetch for Wide Receivers
+            historical_stats = await self._fetch_player_historical_stats(
+                player_id, session, profile.position
+            )
+
         return SleeperPlayer(
-            player_id=player_id, player_profile=profile, player_news=news
+            player_id=player_id,
+            player_profile=profile,
+            player_news=[],
+            historical_stats=historical_stats,
         )
+        # news = await self._fetch_player_news(player_id, session)
+        # return SleeperPlayer(
+        #     player_id=player_id, player_profile=profile, player_news=news
+        # )
 
     async def _fetch_player_data(self):
         async with aiohttp.ClientSession() as session:
@@ -45,6 +68,7 @@ class SleeperAPIClient:
                         for player_id, player_info in data.items()
                         if filter_players(player_info)
                     ]
+                    tasks = tasks[:10]
                     self.players = await asyncio.gather(*tasks)
                 else:
                     raise Exception(f"Failed to fetch /players/nfl: {response.status}")
@@ -76,6 +100,64 @@ class SleeperAPIClient:
                 return extracted_news
             else:
                 print(f"Failed to fetch player news for {player_id}: {response.status}")
+
+    async def _fetch_player_historical_stats(
+        self, player_id, session, position: str
+    ) -> SleeperHistoricalStats:
+        print(f"Fetching historical stats for {player_id}")
+        historical_stats = SleeperHistoricalStats(player_id=player_id)
+
+        for year in range(2019, 2024):
+            yearly_stats = (
+                await self._fetch_player_yearly_stats(
+                    player_id, session, year, position
+                )
+                or None
+            )
+            weekly_stats = []
+
+            for week in range(1, 19):
+                week_stats = (
+                    await self._fetch_player_weekly_stats(
+                        player_id, session, year, week, position
+                    )
+                    or None
+                )
+                # print(f"Player {player_id} week {week} stats: {week_stats}")
+                weekly_stats.append(week_stats)
+
+            historical_stats.yearly_stats[year] = yearly_stats
+            historical_stats.weekly_stats[year] = weekly_stats
+
+        return historical_stats
+
+    async def _fetch_player_weekly_stats(
+        self, player_id, session, season: int, week: int, position: str
+    ) -> List[SleeperWRStatsWeekly]:
+        url = f"https://api.sleeper.com/stats/nfl/player/{player_id}?season_type=regular&season={season}&grouping=week"
+
+        async with session.get(url) as response:
+            if response.status == 200:
+                data = await response.json()
+                return extract_stats(data.get(str(week), {}), position, week=week)
+            else:
+                print(
+                    f"Failed to fetch player weekly stats for {player_id}: {response.status}"
+                )
+
+    async def _fetch_player_yearly_stats(
+        self, player_id, session, season: int, position: str
+    ) -> SleeperWRYearlyStats:
+        url = f"https://api.sleeper.com/stats/nfl/player/{player_id}?season_type=regular&season={season}&grouping=year"
+
+        async with session.get(url) as response:
+            if response.status == 200:
+                data = await response.json()
+                return extract_stats(data, position)
+            else:
+                print(
+                    f"Failed to fetch player yearly stats for {player_id}: {response.status}"
+                )
 
     # async def upload_sleeper_data(self, data):
     #     batch = self.db.batch()
--- sleeper_service/src/types.py
+++ sleeper_service/src/types.py
@@ -1,5 +1,5 @@
 from dataclasses import asdict, dataclass, field
-from typing import List, Optional
+from typing import Dict, List, Optional
 
 
 @dataclass
@@ -49,14 +49,106 @@ class SleeperProfile:
 
 
 @dataclass
+class SleeperWRYearlyStats:
+    player_id: str
+    team: str
+    year: int
+    position_rank_half_ppr: int
+    position_rank_ppr: int
+    position_rank_std: int
+    pts_half_ppr: float
+    pts_ppr: float
+    pts_std: float
+    rank_half_ppr: int
+    rank_ppr: int
+    rank_std: int
+    receptions: int
+    receiving_yards: int
+    receiving_touchdowns: int
+    longest_reception: int
+    receiving_targets: int
+    receiving_yards_per_target: float
+    rushing_and_receiving_yards: int
+
+    def to_dict(self):
+        return asdict(self)
+
+
+@dataclass
+class SleeperWRStatsWeekly:
+    player_id: str
+    week: int
+    season: int
+    team: str
+    opponent: str
+    pts_half_ppr: float
+    pts_ppr: float
+    pts_std: float
+    offensive_snaps: int
+    penalties: int
+    penalty_yards: int
+    rec: int
+    rec_0_4_yards: int
+    rec_5_9_yards: int
+    rec_10_19_yards: int
+    rec_20_29_yards: int
+    rec_30_39_yards: int
+    rec_40_plus_yards: int
+    rec_air_yards: int
+    rec_drop: int
+    rec_first_down: int
+    rec_long: int
+    rec_targets: int
+    rec_redzone_targets: int
+    rec_td: int
+    rec_td_long: int
+    rec_yards: int
+    rec_yac: int
+    rec_yards_per_rec: float
+    rec_yards_per_target: float
+    rush_and_rec_yards: int
+
+    def to_dict(self):
+        return asdict(self)
+
+
+@dataclass
+class SleeperHistoricalStats:
+    player_id: str
+    yearly_stats: Dict[int, Optional[SleeperWRYearlyStats]] = field(
+        default_factory=dict
+    )
+    weekly_stats: Dict[int, List[Optional[SleeperWRStatsWeekly]]] = field(
+        default_factory=dict
+    )
+
+    def to_dict(self):
+        return {
+            "player_id": self.player_id,
+            "yearly_stats": {
+                year: (y_stats.to_dict() if y_stats else {})
+                for year, y_stats in self.yearly_stats.items()
+            },
+            "weekly_stats": {
+                year: [w_stats.to_dict() if w_stats else {} for w_stats in week_stats]
+                for year, week_stats in self.weekly_stats.items()
+            },
+        }
+
+
+@dataclass
 class SleeperPlayer:
     player_id: str
     player_profile: SleeperProfile
     player_news: List[SleeperNews]
+    historical_stats: Optional[SleeperHistoricalStats] = None
 
     def to_dict(self):
         return {
             "player_id": self.player_id,
             "player_profile": self.player_profile.to_dict(),
             "player_news": [news.to_dict() for news in self.player_news],
+            "historical_stats": (
+                self.historical_stats.to_dict() if self.historical_stats else {}
+            ),
         }
